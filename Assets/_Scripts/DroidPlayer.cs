fdfde604ec83533ca932b5cc14fcf91c5480b8a1
Kevin <kevinlardner96@gmail.com>
2017-10-11 17:04:52 +0100

    Added 2 script files

---
+++ Assets/_Scripts/DroidPlayer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * @Class DroidPlayer sets droid status
+ * @auther Kevin Lardner | B00078871
+ * 
+ */
+public class DroidPlayer : MonoBehaviour {
+    /**
+     * Location = droid status - flying, recharging, mining
+     * Energy = droid is recharged TRUE or FALSE
+     */
+    private DroidPlayer miningDroid;
+    public bool location;
+    public bool energy;
+
+	/*
+     * initialization
+     */
+	void Awake () {
+        miningDroid = GetComponent<DroidPlayer>();
+    }
+	
+    /*
+     * Updates Droid Status
+     */
+	public void UpdateDisplay ()
+    {
+        bool location = miningDroid.GetLocation();
+        bool energy = miningDroid.GetEnergyStatus();
+
+        string msg = "Droid is recharged: = " + energy + " | Droid is currently: = " + location;
+        print(msg);
+    }
+
+    /**
+     * @Method retrieves drone location
+     * @return location
+     */
+    public bool GetLocation()
+    {
+        return location;
+    }
+
+    /**
+     * @Method retrieves drone energy level
+     * @return energy
+     */
+    public bool GetEnergyStatus()
+    {
+        return energy;
+    }
+}
