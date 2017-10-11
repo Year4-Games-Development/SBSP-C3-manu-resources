c684a0062c5de6dcf5ce3a1bec755093e33625f9
Kevin <kevinlardner96@gmail.com>
2017-10-11 17:36:25 +0100

    Pull all files inside SBSP folder

---
+++ Assets/Components/C1_ship_main/Scripts/ShipController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ShipController : MonoBehaviour 
+{
+    private ShipModel shipModel;
+
+	void Awake () 
+    {
+        shipModel = new ShipModel();
+	}
+
+    public ShipModel GetShipModel()
+    {
+        return shipModel;
+    }	
+}
