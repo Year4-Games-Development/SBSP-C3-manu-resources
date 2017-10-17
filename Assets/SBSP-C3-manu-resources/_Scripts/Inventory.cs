d6a95b9b7f2e2b60137a4aabd4edb914677c1b6e
Fred Smith <Fred Smith>
2017-10-17 17:06:22 +0100

    Updated Drag and Drop system for inventory

--- Assets/SBSP-C3-manu-resources/_Scripts/Inventory.cs
+++ Assets/SBSP-C3-manu-resources/_Scripts/Inventory.cs
@@ -25,6 +25,7 @@ public class Inventory : MonoBehaviour {
         {
             items.Add(new Item());
             slots.Add(Instantiate(inventorySlot));
+            slots[i].GetComponent<ItemSlot>().id = i;
             slots[i].transform.SetParent(slotPanel.transform);
         }
 
@@ -62,6 +63,7 @@ public class Inventory : MonoBehaviour {
                     items[i] = itemToAdd;
                     GameObject itemObj = Instantiate(inventoryItem);
                     itemObj.GetComponent<ItemData>().item = itemToAdd;
+                    itemObj.GetComponent<ItemData>().slot = i;
                     itemObj.transform.SetParent(slots[i].transform);
                     itemObj.transform.position = Vector2.zero;
                     itemObj.GetComponent<Image>().sprite = itemToAdd.Sprite;
--- Assets/SBSP-C3-manu-resources/_Scripts/ItemData.cs
+++ Assets/SBSP-C3-manu-resources/_Scripts/ItemData.cs
@@ -8,22 +8,35 @@ public class ItemData : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
 {
     public Item item;
     public int amount;
+    public int slot;
+
+    private Transform originalParent;
+    private Vector2 offset;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         if (item.ID != -1)
         {
-            this.transform.position = eventData.position;
+            offset = eventData.position - new Vector2(this.transform.position.x, this.transform.position.y);
+            originalParent = this.transform.parent;
+            this.transform.SetParent(this.transform.parent.parent);
+            this.transform.position = eventData.position - offset;
+            GetComponent<CanvasGroup>().blocksRaycasts = false;
         }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        throw new NotImplementedException();
+        if (item.ID != -1)
+        {
+            this.transform.position = eventData.position - offset;
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        throw new NotImplementedException();
+        this.transform.SetParent(originalParent);
+        this.transform.position = originalParent.transform.position;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
     }
 }
