7c286a50295601b3e7a9808e22367f6dfde11d1a
Fred Smith <Fred Smith>
2017-10-18 16:17:23 +0100

    Inventory updated 18/10/2017

--- Assets/SBSP-C3-manu-resources/_Scripts/Inventory.cs
+++ Assets/SBSP-C3-manu-resources/_Scripts/Inventory.cs
@@ -63,6 +63,7 @@ public class Inventory : MonoBehaviour {
                     items[i] = itemToAdd;
                     GameObject itemObj = Instantiate(inventoryItem);
                     itemObj.GetComponent<ItemData>().item = itemToAdd;
+                    itemObj.GetComponent<ItemData>().amount = 1;
                     itemObj.GetComponent<ItemData>().slot = i;
                     itemObj.transform.SetParent(slots[i].transform);
                     itemObj.transform.position = Vector2.zero;
--- Assets/SBSP-C3-manu-resources/_Scripts/ItemData.cs
+++ Assets/SBSP-C3-manu-resources/_Scripts/ItemData.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using System;
 
-public class ItemData : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class ItemData : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public Item item;
     public int amount;
     public int slot;
 
-    private Transform originalParent;
+    private Inventory inv;
+    private ToolTip toolTip;
     private Vector2 offset;
+
+    void Start()
+    {
+        inv = GameObject.Find("Inventory").GetComponent<Inventory>();
+        toolTip = inv.GetComponent<ToolTip>();
+    }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         if (item.ID != -1)
         {
             offset = eventData.position - new Vector2(this.transform.position.x, this.transform.position.y);
-            originalParent = this.transform.parent;
             this.transform.SetParent(this.transform.parent.parent);
             this.transform.position = eventData.position - offset;
             GetComponent<CanvasGroup>().blocksRaycasts = false;
@@ -35,8 +41,18 @@ public class ItemData : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        this.transform.SetParent(originalParent);
-        this.transform.position = originalParent.transform.position;
+        this.transform.SetParent(inv.slots[slot].transform);
+        this.transform.position = inv.slots[slot].transform.position;
         GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        toolTip.Activate(item);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        toolTip.Deactivate();
     }
 }
