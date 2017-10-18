aa901d3d9cd4149e369b9980e55f7b517b59b3be
OwenFlannery <owen.flannery22@gmail.com>
2017-10-18 11:36:05 +0100

    manufcture tab done using ui text

---
+++ Assets/SBSP-C3-manu-resources/_Scripts/ManuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManuController : MonoBehaviour {
+
+    private ManuModel manuModel;
+
+    // Use this for initialization
+    void Awake()
+    {
+        manuModel = new ManuModel();
+    }
+
+    public ManuModel GetManuModel()
+    {
+        return manuModel;
+    }
+
+}
---
+++ Assets/SBSP-C3-manu-resources/_Scripts/ManuView.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManuView : MonoBehaviour
+{
+    /// <summary>
+    /// /science upgrades
+    /// </summary>
+    public Text munufactureText;
+    public Text fuel;
+    public Text rockets;
+    public Text ammo;
+
+    private ManuController manuController;
+    private ManuModel manuModel;
+
+    void Awake()
+    {
+        manuController = GetComponent<ManuController>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        manuModel = manuController.GetManuModel();
+        UpdateViews();
+    }
+
+    void Update()
+    {
+        UpdateViews();
+    }
+
+    private void UpdateViews()
+    {
+        ///science setters 
+        munufactureText.text = manuModel.GetManufactureText();
+        fuel.text = manuModel.GetFuel();
+        rockets.text = manuModel.GetRockets();
+        ammo.text = manuModel.GetAmmo();
+
+    }
+}
