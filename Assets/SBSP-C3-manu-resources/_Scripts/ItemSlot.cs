1ac4cceb6fd2da762c96cdf84a6e7f47c0680b19
MindaugasKluonis <9nsGM2CsB>
2017-10-18 16:21:16 +0100

    Updating my fork

---
+++ Assets/SBSP-C3-manu-resources/_Scripts/ItemSlot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System;
+
+public class ItemSlot : MonoBehaviour, IDropHandler {
+    public int id;
+    private Inventory inv;
+
+    void Start()
+    {
+        inv = GameObject.Find("Inventory").GetComponent<Inventory>();
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        ItemData droppedItem = eventData.pointerDrag.GetComponent<ItemData>();
+        Debug.Log("fead");
+        if(inv.items[id].ID == -1)
+        {
+            droppedItem.transform.SetParent(this.transform);
+            droppedItem.transform.position = this.transform.position;
+        }
+    }
+}
