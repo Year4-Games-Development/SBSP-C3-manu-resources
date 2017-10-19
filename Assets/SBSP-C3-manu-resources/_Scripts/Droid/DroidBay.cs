2928fb9cf349dd7ebc0ecca78cdbfc05f27c0f1d
MindaugasKluonis <9nsGM2CsB>
2017-10-19 19:50:34 +0100

    Created dynamic droid bay that stores all available droids + added new design to droid UI

--- Assets/SBSP-C3-manu-resources/_Scripts/Droid/DroidBay.cs
+++ Assets/SBSP-C3-manu-resources/_Scripts/Droid/DroidBay.cs
@@ -2,31 +2,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class DroidBay
+public class DroidBay : MonoBehaviour
 {
+    public Button deployButton;
+    public Button upgradeButton;
+    public Button removeButton;
+
     private DroidBayModel _droidBayModel;
     private DroidBayView _droidBayView;
 
-    public DroidBay()
+    void Awake()
     {
-
         _droidBayModel = new DroidBayModel();
-        _droidBayView = new DroidBayView();
 
+        deployButton.onClick.AddListener(DeployDroid);
+        upgradeButton.onClick.AddListener(UpgradeDroid);
+        removeButton.onClick.AddListener(RemoveDroid);
+
+        _droidBayView = new DroidBayView(deployButton, upgradeButton, removeButton);
     }
 
-    public DroidBayModel GetDroidModel()
+    public DroidBayModel GetDroidBayModel()
     {
-
         return _droidBayModel;
-
     }
 
     public DroidBayView GetDroidView()
     {
-
         return _droidBayView;
+    }
+
+    public void DeployDroid()
+    {
+
+        Debug.Log("Deploying from: " + _droidBayModel.GetBayIndex());
+
+    }
+
+    public void UpgradeDroid()
+    {
+
+        Debug.Log("Upgrading from: " + _droidBayModel.GetBayIndex());
+
+    }
+
+    public void RemoveDroid()
+    {
+
+        Debug.Log("Removing from: " + _droidBayModel.GetBayIndex());
 
     }
 }
---
+++ Assets/SBSP-C3-manu-resources/_Scripts/Droid/DroidManagerView.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DroidManagerView{
+
+    private Button _createNewBayButton;
+
+    public DroidManagerView(Button button)
+    {
+
+        _createNewBayButton = button;
+
+    }
+
+}
