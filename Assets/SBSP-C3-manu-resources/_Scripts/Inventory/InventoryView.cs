841c73587537946c75f29a38482402fc91a59fe8
CJohnGitHub <charlesjhn1@gmail.com>
2017-10-23 11:25:48 +0100

    invintory restructure

---
+++ Assets/SBSP-C3-manu-resources/_Scripts/Inventory/InventoryView.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryView : MonoBehaviour {
+
+    public GameObject slotsPanel;
+    public GameObject slot;
+    public GameObject slotItem;
+    public List<GameObject> slotsList = new List<GameObject>();
+}
