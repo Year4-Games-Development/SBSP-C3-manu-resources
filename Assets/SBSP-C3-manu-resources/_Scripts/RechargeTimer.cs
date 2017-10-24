b6088929c1065b3c22eb2a248a3c19362a12a7d3
MindaugasKluonis <9nsGM2CsB>
2017-10-24 12:31:28 +0100

    Droids can be deployed

--- Assets/SBSP-C3-manu-resources/_Scripts/RechargeTimer.cs
+++ Assets/SBSP-C3-manu-resources/_Scripts/RechargeTimer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +8,21 @@ public class RechargeTimer
 
     private float countdownTimerStartTime;
     private int countdownTimerDuration;
+    private int seconds;
+
+    private IEnumerator couroutine;
+
+    public RechargeTimer()
+    {
+
+    }
+
+    public RechargeTimer(int timer)
+    {
+
+        countdownTimerDuration = timer;
+
+    }
     
     public int GetTotalSeconds()
     {
@@ -36,5 +52,44 @@ public class RechargeTimer
     {
         float proportionLeft = (float)GetSecondsRemaining() / (float)GetTotalSeconds();
         return proportionLeft;
+    }
+
+    private IEnumerator DecrementTimer(int seconds, ITimeable callback)
+    {
+
+        this.seconds = seconds;
+
+        callback.OnStartTimer();
+
+        while (this.seconds > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            this.seconds--;
+            callback.OnIncrementTimer();
+        }
+
+        callback.OnFinishTimer();
+
+    }
+
+    public IEnumerator GetCurrentCouroutine()
+    {
+        return couroutine;
+    }
+
+    public IEnumerator StartTimerCouroutine(int seconds, ITimeable callback)
+    {
+
+        couroutine = DecrementTimer(seconds, callback);
+
+        return couroutine;
+
+    }
+
+    public int GetRemainingSeconds()
+    {
+
+        return seconds;
+
     }
 }
