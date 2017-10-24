abb0db085cfb9f37a41f38860541dcd036d0d0ff
CJohnGitHub <charlesjhn1@gmail.com>
2017-10-24 12:12:57 +0100

    New Inventory restructure

---
+++ Assets/SBSP-C3-manu-resources/_Scripts/Inventory/RemoveItem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class RemoveItem : InventoryHelper {
+
+    public void UseItem()
+    {
+        //Getting child count.
+        int count = gameObject.transform.childCount;
+
+        if (count == 1)
+        {
+            // Getting slot id
+            int itemSlotId = gameObject.GetComponent<ItemSlot>().id;
+
+            //Getting game object child
+            GameObject go = gameObject.transform.GetChild(0).gameObject;
+
+            //Getting item name
+            string itemName = go.GetComponent<ItemData>().Name;
+
+            //Remove the item.
+            inventoryManager.RemoveItem(itemSlotId, itemName);
+        }
+    }
+}
