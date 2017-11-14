db9b677e2cd0982e0f23be5e8d2dcf68d71891fc
OwenFlannery <owen.flannery22@gmail.com>
2017-11-14 21:11:31 +0000

    research controller

--- Assets/SBSP-C3-manu-resources/_Scripts/research/Research.cs
+++ Assets/SBSP-C3-manu-resources/_Scripts/research/Research.cs
@@ -1,5 +1,5 @@
-﻿public class Research{
-
+﻿public class Research
+{
     private string _researchName;
     private string _description;
 
@@ -8,89 +8,64 @@
 
     AllResearches _thisResearch;
     
-
     public Research(string name, string description, AllResearches research, int time)
     {
-
         _researchName = name;
         _description = description;
         _thisResearch = research;
         _isLearned = false;
         _timeToLearn = time;
-
     }
 
     public bool IsLearned()
     {
-
         return _isLearned;
-
     }
 
     public AllResearches GetResearch()
     {
-
         return _thisResearch;
-
     }
 
     public void SetLearned(bool value)
     {
-
         _isLearned = value;
-
     }
 
     public void SetName(string name)
     {
-
         _researchName = name;
-
     }
 
     public void SetDescription(string description)
     {
-
         _description = description;
-
     }
 
     public string GetName()
     {
-
         return _researchName;
-
     }
 
     public string GetDescription()
     {
-
         return _description;
-
     }
 
     public void SetTime(int value)
     {
-
         _timeToLearn = value;
-
     }
 
     public int GetTime()
     {
-
         return _timeToLearn;
-
     }
-
-
 }
 
 public enum AllResearches
 {
-
     More_Bays,
     Machinegun,
     Rockets
-
 }
--- Assets/SBSP-C3-manu-resources/_Scripts/research/ResearchPanelController.cs
+++ Assets/SBSP-C3-manu-resources/_Scripts/research/ResearchPanelController.cs
@@ -3,10 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResearchPanelController : MonoBehaviour {
-
+public class ResearchPanelController : MonoBehaviour
+{
     public GameObject researchPrefab;
-
     private List<Research> _researches;
 
     void Awake()
@@ -15,30 +14,24 @@ public class ResearchPanelController : MonoBehaviour {
         _researches.Add(new Research("More Bays", "You can build more bays", AllResearches.More_Bays, 20));
         _researches.Add(new Research("Machineguns", "You can build machineguns", AllResearches.Machinegun, 15));
         _researches.Add(new Research("Rockets", "Learn to make rockets", AllResearches.Rockets, 60));
-
     }
 
     void Start()
     {
-
         //generate UI for each research
         GenerateResearches();
-
     }
 
     private void GenerateResearches()
     {
-
         for (int i = 0; i < _researches.Count; i++)
         {
-
             GameObject newResearchGameObject = Instantiate(researchPrefab);
             newResearchGameObject.transform.SetParent(gameObject.transform);
             newResearchGameObject.transform.localScale = new Vector3(1, 1, 1);
 
             ResearchController researchController = newResearchGameObject.GetComponent<ResearchController>();
             researchController.GetResearchModel().SetResearch(_researches[i]);
-
         }
     }
 
